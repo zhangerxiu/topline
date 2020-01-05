8a430db1eb18616e56355f88632b11673cc4f2a3
zhangerxiu <1714161982@qq.com>
2020-01-05 19:49:29 +0800

    引入了按钮

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
 
+// import  from 'element-ui'
+
+// // 引入css样式
+// import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI, { Row, Button, MessageBox, Message } from 'element-ui'
+Vue.use(ElementUI)
+// 按需注册组件
+Vue.use(Row)
+Vue.use(Button)
+// 按需注册事件方法(原型继承)
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$message = Message
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
